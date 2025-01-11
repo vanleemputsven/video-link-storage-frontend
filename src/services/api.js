4eb1b9fc299cbb6c84d995923c5b7022c0ee49c9
vanleemputsven <121162637+vanleemputsven@users.noreply.github.com>
2025-01-11 21:26:49 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
-  withCredentials: true,
+  baseURL: "https://video-link-storage.onrender.com/api",
 });
 
 // Voeg CancelToken toe voor upload annuleren
